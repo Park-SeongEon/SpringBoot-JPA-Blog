3d7e1ca284af6ab8c148589a0f8df5e4233442de
one031411 <one031411@naver.com>
2023-08-04 12:03:41 +0900

    lombok 테스트

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,20 +12,20 @@ public class HttpControllerTest {
 	
 	//http://localhost:8080/http/get(select)
 	@GetMapping("http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {
+		return "get 요청:"+ m.getId()+","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	
 	//http://localhost:8080/http/post(insert)
-	@PostMapping("http/post")
-	public String postTest() {
-		return "post 요청";
+	@PostMapping("http/post") //text/plain, application/json
+	public String postTest(@RequestBody Member m) { //MessageConverter(스프링부트)
+		return "post 요청:" +m.getId()+","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	
 	//http://localhost:8080/http/put(update)
 	@PutMapping("http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청:" +m.getId()+","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	
 	//http://localhost:8080/http/delete(delete)
@@ -32,4 +33,15 @@ public class HttpControllerTest {
 	public String deleteTest() {
 			return "delete 요청";
 		}
+	
+	private static final String TAG="HttpControllerTest:";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+"getter:"+m.getId());
+		m.setId(5000);
+		System.out.println(TAG+"getter:"+m.getId());
+		return "lombok test 완료";
+	}
 }
